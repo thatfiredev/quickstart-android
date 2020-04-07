00376d88a574fbd6a1120feb471b0b7a08ece528
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2020-04-07 14:14:59 +0200

    chore: implement conditional builds in Pull Requests

--- build_pull_request.sh
+++ build_pull_request.sh
@@ -24,8 +24,38 @@ cp mock-google-services.json mlkit-smartreply/app/google-services.json
 cp mock-google-services.json mlkit-translate/app/google-services.json
 cp mock-google-services.json storage/app/google-services.json
 
+
+AVAILABLE_TASKS=$(./gradlew tasks --all)
+changed_modules=""
+
+# Check which modules were changed
+git diff --name-only | { while read line
+  do
+    module_name=${line%%/*}
+    if [[ ${changed_modules} != *"$module_name"* ]]; then
+      changed_modules="${changed_modules} ${module_name}"
+    fi
+  done
+}
+
+# Check if these modules have gradle tasks
+build_commands=""
+for module in $changed_modules
+do
+  if [[ $AVAILABLE_TASKS =~ $module":app:" ]]; then
+    build_commands="${build_commands} :${module}:app:assembleDebug :${module}:app:check"
+  fi
+done
+
+if [[ $build_commands == "" ]]; then
+  # The changes were made in directories with no gradle tasks
+  # Let's build debug, just in case
+  build_commands="assembleDebug check"
+fi
+
 # Build
 echo "Building Pull Request..."
 # On a pull request, just build debug which is much faster and catches
 # obvious errors.
-./gradlew clean ktlint assembleDebug check
+./gradlew clean ktlint
+eval $build_commands
