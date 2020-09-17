a0df9c3b4f5f5bcab2a3e9bac7ccfc6dd1aca670
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2020-09-17 16:42:52 +0200

    refactor: test going full js

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const shell = require('shelljs')
+const shell = require('shelljs');
 const core = require('@actions/core');
+const github = require('@actions/github');
 
 let forAllModules = core.getInput('for-all-modules');
 let forEachModule = core.getInput('for-each-module');
 
-shell.exec('./entrypoint.sh ' + forEachModule + ' ' + forAllModules)
+// unshallow since GitHub actions does a shallow clone
+shell.exec('git fetch --unshallow');
+shell.exec('git fetch origin');
+
+// Get all changed modules
+
+const baseBranch = 'origin/' + process.env.GITHUB_BASE_REF;
+shell.exec('git diff --name-only ', function (code, stdout, stderr) {
+    console.log('Exit code:', code);
+    console.log('Std Out:', stdout);
+    console.log('Std Error:', stderr);
+});
