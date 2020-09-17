4eb30d6ef05cbc2c8549a988ffcddc9fb816d0cc
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2020-09-17 16:47:07 +0200

    chore: add branch to git diff

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ shell.exec('git fetch origin');
 // Get all changed modules
 
 const baseBranch = 'origin/' + process.env.GITHUB_BASE_REF;
-shell.exec('git diff --name-only ', function (code, stdout, stderr) {
+shell.exec('git diff --name-only ' + baseBranch, function (code, stdout, stderr) {
     console.log('Exit code:', code);
     console.log('Std Out:', stdout);
     console.log('Std Error:', stderr);
